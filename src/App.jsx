3d839ece287e99598c08ee7105d0a3ce52fa65ae
Nikesh <nikeshkhatri763@gmail.com>
2024-01-02 23:52:26 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+	// const [first, setfirst] = useState(second)
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		const data = new FormData(e.target);
+		let enterdedData = Object.fromEntries(data.entries());
+		console.log("🚀 ~ file: App.jsx:8 ~ handleFormSubmit ~ enterdedData:", enterdedData);
+	};
+
+	document.addEventListener(
+		"invalid",
+		(function () {
+			return function (e) {
+				e.preventDefault();
+				e.target.focus();
+			};
+		})(),
+		true
+	);
+
+	return (
+		<>
+			<div className="FormWrapper">
+				<div className="contentWrapper">
+					<h1>Testing form validadtion with css only</h1>
+					<form onSubmit={handleFormSubmit} className="TestFomrm">
+						<div className="inputWrapper">
+							<label htmlFor="firstName"> First name</label>
+							<input
+								type="text"
+								name="firstName"
+								id="firstName"
+								required
+								placeholder="First Name"
+								pattern="^[a-zA-Z ]{3,}$"
+								title="Only alphabet between 3 and 30 characters are allowed."
+							/>
+							<h2 className="ReqMessage">Required.</h2>
+							<h2 className="ErrorMessage">Only alphabet between 3 and 30 characters are allowed.</h2>
+						</div>
+						<div className="inputWrapper">
+							<label htmlFor="lastName"> Last name</label>
+							<input
+								type="text"
+								name="lastName"
+								id="lastName"
+								placeholder="Last Name"
+								required
+								pattern="^[a-zA-Z ]{3,}$"
+								title="Only alphabet between 3 and 30 characters are allowed."
+							/>
+							<h2 className="ReqMessage">Required.</h2>
+							<h2 className="ErrorMessage">Only alphabet between 3 and 30 characters are allowed.</h2>
+						</div>
+						<div className="inputWrapper">
+							<label htmlFor="email">Email</label>
+							<input type="email" name="email" id="email" required placeholder="Email" title="Please enter a valid email" />
+							<h2 className="ReqMessage">Required.</h2>
+							<h2 className="ErrorMessage">Please enter a valid email</h2>
+						</div>
+						<div className="inputWrapper">
+							<label htmlFor="contact">Phone number</label>
+							<input type="number" name="contact" id="contact" placeholder="contact" />
+						</div>
+						<button>Submit</button>
+					</form>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default App;
